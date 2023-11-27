fa31cdbf04cfbf8fb513f47860a03e031e393182
Samuel Brucksch <SamuelBrucksch@users.noreply.github.com>
2023-11-27 09:19:50 +0100

    Blubb (#27)
    * ignore battery cost again (for now)
    
    * add idle state
    
    ---------
    
    Co-authored-by: Samuel Brucksch <samuel@iternio.com>

--- test/strategy-battery-charging-functions-mutate.test.js
+++ test/strategy-battery-charging-functions-mutate.test.js
@@ -1,28 +1,50 @@
-const { mockRandomForEach } = require('jest-mock-random')
+const { mockRandomForEach, mockRandom } = require('jest-mock-random')
 const {
   mutationFunction
 } = require('../src/strategy-battery-charging-functions')
 
 describe('Mutation', () => {
-  mockRandomForEach(0.4)
+  // mockRandomForEach(0.4)
 
-  test('should mutate', () => {
+  test('should mutate to idle', () => {
+    mockRandom(1 / 3)
     const mutate = mutationFunction({ totalDuration: 120, mutationRate: 1 })
 
     const p = mutate({
-      periods: [
-        { start: 0, activity: 1, duration: 10 },
-        { start: 90, activity: -1, duration: 10 }
-      ],
+      periods: [{ start: 0, activity: 1, duration: 10 }],
       excessPvEnergyUse: 0
     })
 
     expect(p).toMatchObject({
-      periods: [
-        { start: 0, activity: -1, duration: 10 },
-        { start: 90, activity: 1, duration: 10 }
-      ],
+      periods: [{ start: 0, activity: 0, duration: 10 }]
+    })
+  })
+
+  test('should mutate to discharging', () => {
+    mockRandom(2 / 3)
+    const mutate = mutationFunction({ totalDuration: 120, mutationRate: 1 })
+
+    const p = mutate({
+      periods: [{ start: 0, activity: 1, duration: 10 }],
       excessPvEnergyUse: 0
+    })
+
+    expect(p).toMatchObject({
+      periods: [{ start: 0, activity: -1, duration: 10 }]
+    })
+  })
+
+  test('should mutate to charging', () => {
+    mockRandom(0.9)
+    const mutate = mutationFunction({ totalDuration: 120, mutationRate: 1 })
+
+    const p = mutate({
+      periods: [{ start: 0, activity: -1, duration: 10 }],
+      excessPvEnergyUse: 0
+    })
+
+    expect(p).toMatchObject({
+      periods: [{ start: 0, activity: 1, duration: 10 }]
     })
   })
 })
